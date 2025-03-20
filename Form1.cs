d8c756495aba5083c3bd1fec59b24e6aebcd1a81
akmaalsyh <taufansyah50@gmail.com>
2025-03-20 23:38:49 +0700

    mengisi nilai txtEmail dari DataGridView

--- Form1.cs
+++ Form1.cs
@@ -170,6 +170,7 @@ namespace CRUDSederhana
                 // Coba gunakan indeks jika "NIM" tidak ditemukan
                 txtNIM.Text = row.Cells[0].Value?.ToString();
                 txtNama.Text = row.Cells[1].Value?.ToString();
+                txtEmail.Text = row.Cells[2].Value?.ToString();
             }
         }
     }
