b125f0ead17df935c541e37db2c26a2b59fd0e68
akmaalsyh <taufansyah50@gmail.com>
2025-03-20 22:44:16 +0700

    menambahkan fungsi ClearForm untuk mengosongkan input pada textbox

--- Form1.cs
+++ Form1.cs
@@ -27,7 +27,11 @@ namespace CRUDSederhana
             LoadData();
         }
 
+        // Fungsi untuk mengosongkan semua input pada TextBox
+        private void ClearForm()
+        {
 
+        }
         private void label1_Click_1(object sender, EventArgs e)
         {
 
